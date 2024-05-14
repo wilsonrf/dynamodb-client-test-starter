7d91e42b7975157f94a79f4ca8d4ea2892832062
Wilson da Rocha Franca <wilsonrf@gmail.com>
2024-05-14 17:48:52 +0200

    setup build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,44 @@
 plugins {
-    id("java")
+    id("java-library")
+    id("org.springframework.boot") apply false
+    id("io.spring.dependency-management")
+    id("com.github.ben-manes.versions")
+    id("maven-publish")
 }
 
 group = "com.wilsonfranca"
-version = "1.0-SNAPSHOT"
+version = "1.0.0-SNAPSHOT"
 
 repositories {
+    mavenLocal()
     mavenCentral()
 }
-
+dependencyManagement {
+    imports {
+        mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
+    }
+}
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    api("com.wilsonfranca:dynamodb-client-autoconfigure-test:1.0.0-SNAPSHOT")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+        }
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,13 @@
-rootProject.name = "dynamodb-client-autoconfigure-test-starter"
+rootProject.name = "dynamodb-client-test-starter"
 
+pluginManagement {
+    val springBootVersion: String by settings
+    val springDependencyManagementVersion: String by settings
+    val dependencyManagementVersion: String by settings
+
+    plugins {
+        id("org.springframework.boot") version springBootVersion
+        id("io.spring.dependency-management") version springDependencyManagementVersion
+        id("com.github.ben-manes.versions") version dependencyManagementVersion
+    }
+}
